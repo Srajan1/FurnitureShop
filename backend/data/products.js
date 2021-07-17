ce88acdd31981c20fd3f1f89f784bbb7c348e6f1
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-07-17 09:34:08 +0530

    setting up the backend

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Bedsite table',
+    image: '/images/bedsidetable.jpeg',
+    description:
+      'A nice and cozy bedside table for you to keep your alarm clock or that favorite book.',
+    brand: 'Ikea',
+    category: 'Room Decor',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Chair',
+    image: '/images/chair.jpeg',
+    description:
+      'A nice chair cum mini sofa for those lazy evening music sessions or just chilling out with your friends',
+    brand: 'Ikea',
+    category: 'Living Room',
+    price: 59.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Coffee Table',
+    image: '/images/coffeetable.jpeg',
+    description:
+      'A perfect coffee table for your perfect coffee',
+    brand: 'Kitchen decor',
+    category: 'Kitchen decor',
+    price: 29.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Living Room Set',
+    image: '/images/livingroomset.jpeg',
+    description:
+      'Complete set for your living room, that will match your vibe',
+    brand: 'Ikea',
+    category: 'Living room',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Round table',
+    image: '/images/roundtable.jpeg',
+    description:
+      'A nice round table for a perfect date night❤❤',
+    brand: 'Ram',
+    category: 'Kitchen decor',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Sofa',
+    image: '/images/sofa.jpeg',
+    description:
+      'A nice and cozy sofa for your whole family',
+    brand: 'Ikea',
+    category: 'Sofa',
+    price: 229.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports = products;
